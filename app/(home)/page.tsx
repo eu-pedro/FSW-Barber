3303831294487c25b0e8eae2d41ae102c2bdc8c7
pedro henrique <pedro.henrike2812@gmail.com>
2024-01-31 11:27:49 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,21 @@
 import { Header } from '../_components/Header'
 
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Pedro!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   )
 }
