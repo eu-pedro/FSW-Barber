53720f4908e670dc176c6ba745f21062f575ebf7
pedro henrique <pedro.henrike2812@gmail.com>
2024-02-08 14:09:39 -0300

    feat: add name to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -35,7 +35,11 @@ export default async function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, Pedro!</h2>
+        <h2 className="text-xl font-bold">
+          {session?.user
+            ? `Olá, ${session.user.name?.split(' ')[0]}`
+            : 'Olá, vamos agendar um corte hoje?!'}
+        </h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
