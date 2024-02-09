31403a70ca8c1fe8f840bf2ce1b7a1ae2f0e5e0c
pedro henrique <pedro.henrike2812@gmail.com>
2024-02-09 10:36:40 -0300

    feat: ensure recommended barbershops are shown randomly

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -13,23 +13,29 @@ import { Barbershop, Booking } from '@prisma/client'
 export default async function Home() {
   const session = await getServerSession(authOptions)
 
-  const [barbershops, confirmedBookings] = await Promise.all([
-    db.barbershop.findMany({}),
-    session?.user
-      ? db.booking.findMany({
-          where: {
-            userId: (session.user as any).id,
-            date: {
-              gte: new Date(),
+  const [barbershops, recommendedBarbershops, confirmedBookings] =
+    await Promise.all([
+      db.barbershop.findMany({}),
+      db.barbershop.findMany({
+        orderBy: {
+          id: 'asc',
+        },
+      }),
+      session?.user
+        ? db.booking.findMany({
+            where: {
+              userId: (session.user as any).id,
+              date: {
+                gte: new Date(),
+              },
             },
-          },
-          include: {
-            service: true,
-            barbershop: true,
-          },
-        })
-      : Promise.resolve([]),
-  ])
+            include: {
+              service: true,
+              barbershop: true,
+            },
+          })
+        : Promise.resolve([]),
+    ])
 
   return (
     <div>
@@ -87,7 +93,7 @@ export default async function Home() {
         </h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershops.map((barbershop: Barbershop) => (
+          {recommendedBarbershops.map((barbershop: Barbershop) => (
             <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
               <BarbershopItem barbershop={barbershop} />
             </div>
