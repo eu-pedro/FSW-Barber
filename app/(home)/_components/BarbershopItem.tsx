d8e1d1ce313f07651453b293f7cbc87931519ce5
pedro henrique <pedro.henrike2812@gmail.com>
2024-02-09 10:05:47 -0300

    style: update barbershop item top padding

--- app/(home)/_components/BarbershopItem.tsx
+++ app/(home)/_components/BarbershopItem.tsx
@@ -21,7 +21,7 @@ export function BarbershopItem({ barbershop }: BarbershopItemProps) {
 
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
-      <CardContent className="px-1 py-0">
+      <CardContent className="px-1 py-0 pt-1">
         <div className="px-1 w-full h-[159px] relative">
           <div className="absolute top-2 left-2">
             <Badge
